a827b88482cf4d4e1fcd9fe154d18f30dbb6a92b
Carlos Melero <carlos@carlosmelero.com>
2026-02-24 21:15:06 +0100

    sync

--- ex1/ArtifactCard.py
+++ ex1/ArtifactCard.py
@@ -9,29 +9,37 @@ class ArtifactCard(Card):
         super().__init__(name=name, cost=cost, rarity=rarity)
         if durability <= 0:
             raise ValueError("Error: durability must ve positive integer")
-        self.durability = self.durability
+        self.durability = durability
         self.effect = effect
 
     def play(self, game_state: dict) -> dict:
         if self.is_playable(game_state["available_mana"]):
-            result = self.resolve_effect(game_state["targets"])
+            result = {}
             result["card_played"] = self.name
             result["mana_used"] = self.cost
-            result["effect"] = f"Permanent: {self.effect}"            
+            result["effect"] = f"Permanent: {self.effect}"
             game_state["available_mana"] -= self.cost
         else:
             result = {"error": "Not enough mana."}
         return result
 
-    def activate_ability(self) -> dict
-        if self.durability > 0:
-            self.durability -= 1
+    def activate_ability(self) -> dict:
+
+        if self.durability <= 0:
             return {
                 "artifact": self.name,
-                "effect": "+1 mana this turn",
-                "durability_remaining": self.durability,
-                "status": "active"
-                }
-        else
-            {}
+                "effect": None,
+                "durability_remaining": 0,
+                "status": "destroyed"
+            }
 
+        self.durability -= 1
+
+        status = "active" if self.durability > 0 else "destroyed"
+
+        return {
+            "artifact": self.name,
+            "effect": self.effect,
+            "durability_remaining": self.durability,
+            "status": status
+        }
